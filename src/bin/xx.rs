14952f4202ee2ae4b5ca8434f81a391eb345be28
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2020-12-02 17:40:08 +0100

    rename and cleanup template

---
+++ src/bin/xx.rs
@@ -0,0 +1,32 @@
+#![allow(unused_variables, unused_imports)]
+
+use std::time::Instant;
+
+use common::io;
+use std::collections::HashSet;
+
+fn read_input() -> String {
+  io::read_input("", "20XX-DD")
+}
+
+fn solve(input: &str) -> u32 {
+  0
+}
+
+fn main() {
+  let input = read_input();
+  println!("{:?}", input);
+  let time = Instant::now();
+  let solution = solve(&input);
+  println!("solution {:?} {:?}", solution, time.elapsed());
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn test_solve() {
+    assert_eq!(solve(&read_input()), 0);
+  }
+}
