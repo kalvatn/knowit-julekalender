189d35f532fb92e992a68749fa068f380adc254a
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2020-12-06 08:45:59 +0100

    2020-06 'oneliner'

--- src/bin/2020/06.rs
+++ src/bin/2020/06.rs
@@ -1,9 +1,8 @@
-#![allow(unused_variables, unused_imports)]
-
 use std::time::Instant;
 
+use itertools::Itertools;
+
 use common::io;
-use std::collections::HashSet;
 
 fn read_input() -> String {
   io::read_input(
@@ -13,23 +12,38 @@ fn read_input() -> String {
 }
 
 fn solve(input: &str, elves: u32) -> (u32, u32) {
-  let mut packages: Vec<u32> = input
+  input
     .split(",")
     .map(|s| s.parse::<u32>().unwrap())
-    .collect();
-  packages.sort();
-  packages.reverse();
-  let mut opened = 0;
-  let mut items = 0;
-  let mut solution = (0, 0);
-  for i in packages {
-    opened += 1;
-    items += i;
-    if items % elves == 0 {
-      solution = (items / elves, opened);
-    }
-  }
-  solution
+    .sorted()
+    .rev()
+    .scan(0u32, |acc, n| {
+      *acc = *acc + n;
+      Some(*acc)
+    })
+    .enumerate()
+    .collect::<Vec<(usize, u32)>>()
+    .into_iter()
+    .rfind(|(_i, n)| n % elves == 0)
+    .map(|(i, v)| (v / elves, i as u32 + 1))
+    .unwrap()
+  // let packages = input
+  //   .split(",")
+  //   .map(|s| s.parse::<u32>().unwrap())
+  //   .sorted()
+  //   .rev()
+  //   .collect::<Vec<u32>>();
+  // let mut opened = 0;
+  // let mut items = 0;
+  // let mut solution = (0, 0);
+  // for i in packages {
+  //   opened += 1;
+  //   items += i;
+  //   if items % elves == 0 {
+  //     solution = (items / elves, opened);
+  //   }
+  // }
+  // solution
 }
 
 fn main() {
@@ -48,6 +62,6 @@ mod test {
   #[test]
   fn test_solve() {
     assert_eq!(solve(TEST_INPUT, 9), (8, 5));
-    // assert_eq!(solve(&read_input(),127), 0);
+    assert_eq!(solve(&read_input(), 127), (975, 9886));
   }
 }
